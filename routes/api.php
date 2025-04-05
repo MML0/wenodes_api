4f4d4ad2a346a510472005ef7414db0346b40799
MML0 <h1337124lat@gmail.com>
2025-04-05 20:37:49 +0430

    remote migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Artisan; // Import Artisan if not already imported
 
 
 Route::middleware('auth:sanctum')->put('/user', [AuthController::class, 'update']);
@@ -20,3 +21,11 @@ Route::post('/edit_user',[AuthController::class, 'editUser'])->middleware('auth:
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/run-migrations', function (Request $request) {
+    if ($request->input('key') !== 'aosobila') {
+        abort(403);
+    }
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations completed.';
+});
